4015949c03df4055bcc0cae02c529dc211c5d7ad
adavijit <adavijit@yahoo.com>
2020-03-21 13:21:42 +0530

    test

--- src/pages/GoogleSignup.js
+++ src/pages/GoogleSignup.js
@@ -6,38 +6,42 @@ export class GoogleSignup extends Component {
   constructor(props) {
     super(props);
 }
+componentDidMount(){
+  axios.get('./DummyData.json')
+  .then(function(result) {
+      localStorage.setItem("names", JSON.stringify(result.data));
+      // var storedNames = JSON.parse(localStorage.getItem("names"));
+      // storedNames.map((k,n)=>{
+      //     console.log(k)
+      // })
+      console.log('ll')
+      
+  })
+}
   handleClick =() =>{
     var provider = new firebase.auth.GoogleAuthProvider();
-   
+    console.log("user")
     firebase.auth().signInWithPopup(provider).then(function(result) {
-      var token = result.credential.accessToken;
+      // var token = result.credential.accessToken;
       var user = result.user;
-      console.log(user)
+      console.log(user,"user")
       localStorage.setItem('userId', user.uid);
       localStorage.setItem('userEmail', user.email);
       localStorage.setItem('name', user.displayName);
       localStorage.setItem('photo', user.photoURL);
       window.location.reload();
-      axios.get('./DummyData.json')
-      .then(function(result) {
-          localStorage.setItem("names", JSON.stringify(result.data));
-          // var storedNames = JSON.parse(localStorage.getItem("names"));
-          // storedNames.map((k,n)=>{
-          //     console.log(k)
-          // })
-          
-      })
+     
       // ...
     }).catch(function(error) {
       // Handle Errors here.
      
-      var errorCode = error.code;
+      // var errorCode = error.code;
       var errorMessage = error.message;
       // The email of the user's account used.
-      var email = error.email;
+      // var email = error.email;
       console.log(errorMessage,"errrrrr")
       // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
+      // var credential = error.credential;
       // ...
     });
   }
